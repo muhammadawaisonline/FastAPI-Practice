de48d49b44264550b56973e4653bd6d9644d9594
muhammad awais online <muhammadawaisonline00@gmail.com>
2025-01-02 05:57:57 +0500

    Tags

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
+from routers import blog_get
 
 
 app = FastAPI()
 @app.get("/")
 def index():
     return {"message":"Hello world"}
+app.include_router(blog_get.router)
 
 
 
--- routers/blog_get.py
+++ routers/blog_get.py
@@ -6,14 +6,11 @@ router = APIRouter(
     tags=["blog"]
 )
 
+# @router.get("/blog/all")
+# def get_blog2(page=1, page_size: Optional[int] = None):
+#     return {"message": f" all {page_size} blogs on {page} page"}
 
-
-
-@router.get("/blog/all")
-def get_blog2(page=1, page_size: Optional[int] = None):
-    return {"message": f" all {page_size} blogs on {page} page"}
-
-@router.get("/blog/{id}", status_code=status.HTTP_404_NOT_FOUND)
+@router.get("/{id}", status_code=status.HTTP_404_NOT_FOUND)
 def get_blog(id: int, response: Response):
     if id > 5:
         response.status_code = status.HTTP_404_NOT_FOUND
@@ -22,21 +19,16 @@ def get_blog(id: int, response: Response):
         response.status_code = status.HTTP_200_OK
         return {"message": f"blog with id {id}"}
 
-
-
-
-
-
 class BlogType(str, Enum):
     short = "short"
     story = "story"
     howto = "howto"
 
 
-@router.get("/blog/type/{type}")
+@router.get("/type/{type}")
 def blog_type(type: BlogType):
     return {"message": f"blog type {type}"}
 
-@router.get("/blog/{id}/comment/{comment_id}")
+@router.get("/{id}/comment/{comment_id}", tags=["comment"])
 def get_comment(id:int, comment_id:int, valid:bool=True, username: Optional[str]= None ):
     return {"message": f"In blog No.{id} comment {comment_id} is {valid} provided by user {username}"}
