afeec26e0da4ab1dc9108b880d61c1b2c5c55ad4
muhammad awais online <muhammadawaisonline00@gmail.com>
2025-01-02 23:51:13 +0500

    FastApi model

--- routers/blog_post.py
+++ routers/blog_post.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 router = APIRouter(
     prefix="/bloag",
     tags=["blog"]
 )
+
+class BlogModel(BaseModel):
+    pass
 
 @router.post("/new")
 def create_blog():
