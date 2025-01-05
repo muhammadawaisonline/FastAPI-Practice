3d6f11d6189107d95daf426c9b4b0bcbfcb6dde2
muhammad awais online <muhammadawaisonline00@gmail.com>
2025-01-05 22:28:14 +0500

    Body parameter metadata

--- routers/blog_post.py
+++ routers/blog_post.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Query
+from fastapi import APIRouter, Query, Body
 from pydantic import BaseModel
 from typing import Optional
 
@@ -25,10 +25,15 @@ def create_blog(blog: BlogModel, id:int, version:int = 1):
 def create_comment(blog: BlogModel, id:int, comment_id=Query(
 
     title = "Id of the comment",
-    description= "Some description of the comment_id"
-) ):
+    description= "Some description of the comment_id",
+    alias= "commentId"
+), 
+    content:str = Body(...,
+                       min_length=10,
+                       max_length=50) ):
     return {
         "blog": blog,
         "id": id,
-        "comment_id": comment_id
+        "comment_id": comment_id,
+        "content": content
     }
