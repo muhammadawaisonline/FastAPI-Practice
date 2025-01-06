7808b86b89f4c3cc0c24745b68da322ec190cac5
muhammad awais online <muhammadawaisonline00@gmail.com>
2025-01-06 05:46:11 +0500

    Multiple values to a Parameter

--- routers/blog_post.py
+++ routers/blog_post.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, Query, Body
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 
 router = APIRouter(
-    prefix="/bloag",
+    prefix="/blog",
     tags=["blog"]
 )
 
@@ -22,17 +22,19 @@ def create_blog(blog: BlogModel, id:int, version:int = 1):
         "data":blog}
 
 @router.post("/new/{id}/comment")
-def create_comment(blog: BlogModel, id:int, comment_id=Query(
+def create_comment(blog: BlogModel, id:int, comment_id: int =Query(
 
     title = "Id of the comment",
     description= "Some description of the comment_id",
     alias= "commentId"
 ), 
     content:str = Body(...,
-                       regex= "^[a-z\s]*$") ):
+                       regex= "^[a-z\s]*$") ,
+                       v: Optional[List[str]] = Query(["1.0", "1.1", "1.2 "])):
     return {
         "blog": blog,
         "id": id,
         "comment_id": comment_id,
-        "content": content
+        "content": content,
+        "version": v
     }
