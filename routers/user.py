be320f6ebe304ed2b41a4260021f8839a3cb21e8
muhammad awais online <muhammadawaisonline00@gmail.com>
2025-01-11 08:07:34 +0500

    Read Elements from DATAbase

--- routers/user.py
+++ routers/user.py
@@ -18,6 +18,16 @@ router = APIRouter(
 # def create_user(request:UserBase, db:Session = Depends(get_db)):
 #     return db_user.create_user(db, request)
 
+
+
+# Create Ysers in DB
 @router.post("/", response_model=UserDisplay)
 def create_user(request:UserBase, db:Session = Depends(get_db)):
     return db_user.create_user(db, request)
+
+
+# Read all Users
+@router.get("/", response_model= list[UserDisplay])
+def get_all_users(db:Session = Depends(get_db)):
+    return db_user.get_all_users(db)
+
